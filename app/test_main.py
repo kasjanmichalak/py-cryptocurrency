231856d7d5d7cf159e4058b0fa26164d8ace654b
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-12 10:30:09 +0000

    1save

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from unittest import mock
+
+from .main import cryptocurrency_action
+
+
+def test_cryptocurrency_action_buy() -> None:
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction") as mocked_prediction:
+        mocked_prediction.return_value = 106
+        result = cryptocurrency_action(100)
+        assert result == "Buy more cryptocurrency"
+
+
+def test_cryptocurrency_action_sell() -> None:
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction") as mocked_prediction:
+        mocked_prediction.return_value = 94
+        result = cryptocurrency_action(100)
+        assert result == "Sell all your cryptocurrency"
+
+
+def test_cryptocurrency_action_do_nothing() -> None:
+    with mock.patch(
+            "app.main.get_exchange_rate_prediction") as mocked_prediction:
+        mocked_prediction.return_value = 104
+        result = cryptocurrency_action(100)
+        assert result == "Do nothing"
